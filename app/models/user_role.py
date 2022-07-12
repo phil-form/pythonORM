044b1203524768103c0059cba3c1a8e95bde0cec
philippe.stons <philippe.stons@bstorm.be>
2022-07-12 15:52:44 +0200

    [FIX ROLE] inverse userid and roleid fk references

--- app/models/user_role.py
+++ app/models/user_role.py
@@ -3,8 +3,8 @@ from app.models.base_entity import BaseEntity
 
 class UserRole(BaseEntity, db.Model):
     __tablename__ = "userroles"
-    roleid = db.Column(db.ForeignKey("users.userid"), primary_key=True)
-    userid = db.Column(db.ForeignKey("roles.roleid"), primary_key=True)
+    roleid = db.Column(db.ForeignKey("roles.roleid"), primary_key=True)
+    userid = db.Column(db.ForeignKey("users.userid"), primary_key=True)
 
     rel_user = db.relationship('User', back_populates="roles")
     rel_role = db.relationship('Role', back_populates="users")
