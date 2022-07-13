bd8e7989bfd93ee1a89179f0e1f9b36d7720b7b1
clbr-odoo <clbr@odoo.com>
2022-07-13 09:25:22 +0200

    [ADD] authentication for my basket

--- app/controllers/basket_controller.py
+++ app/controllers/basket_controller.py
@@ -1,12 +1,14 @@
 from flask import render_template
 
 from app import app
+from app.decorators.auth_required import auth_required
 from app.services.basket_service import BasketService
 
 basketService = BasketService()
 
 
 @app.route('/users/<int:userid>/basket', methods=["GET"])
+@auth_required(level="ADMIN", or_is_current_user=True)
 def getBasketUser(userid: int):
     basket = basketService.find_one_by(userid=userid)
     return render_template('baskets/user_basket.html', items=basket.items)
