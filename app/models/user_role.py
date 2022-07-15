401ec9b5c085817f13e16d31d95fe0d36c883fce
CrenierAmaury <amaurycrenier@gmail.com>
2022-07-15 13:36:04 +0200

    fix tablename for userroles

--- app/models/user_role.py
+++ app/models/user_role.py
@@ -2,7 +2,7 @@ from app import db
 from app.models.base_entity import BaseEntity
 
 class UserRole(BaseEntity, db.Model):
-    __tablename__ = "user_roles"
+    __tablename__ = "userroles"
     roleid = db.Column(db.ForeignKey("users.userid"), primary_key=True)
     userid = db.Column(db.ForeignKey("roles.roleid"), primary_key=True)
 
