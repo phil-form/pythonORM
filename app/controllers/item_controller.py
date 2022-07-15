79a92efc6335d20f017c5741208cc93bf80cb0b8
philippe.stons <philippe.stons@bstorm.be>
2022-07-15 10:58:24 +0200

    [ITEM ADD/UPDATE]

--- app/controllers/item_controller.py
+++ app/controllers/item_controller.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from app import app
+from app.forms.item.item_form import ItemForm
 from app.services.item_service import ItemService
 
 itemService = ItemService()
@@ -12,3 +13,42 @@ def getItemList():
 @app.route('/items/<int:itemid>')
 def getItemDetails(itemid):
     return render_template('items/details.html', item=itemService.find_one(itemid))
+
+@app.route('/items/add', methods=['GET','POST'])
+def addItem():
+    form = ItemForm(request.form)
+
+    if request.method == 'POST':
+        if form.validate():
+            item = itemService.insert(form)
+
+            return redirect(url_for('getItemList'))
+
+    print(form.errors)
+    form.itemname.data = ''
+    form.itemdescription.data = ''
+    form.itemstock.data = 1
+
+    return render_template('items/add_or_update.html', form=form)
+
+@app.route('/items/update/<int:itemid>', methods=['GET','POST'])
+def updateItem(itemid: int):
+    item = itemService.find_one(itemid)
+
+    if item is None:
+        return redirect(url_for('getItemList'))
+
+    form = ItemForm(request.form)
+
+    if request.method == 'POST':
+        if form.validate():
+            item = itemService.update(itemid, form)
+
+            return redirect(url_for('getItemList'))
+
+    print(form.errors)
+    form.itemname.data = item.itemname
+    form.itemdescription.data = item.itemdescription
+    form.itemstock.data = item.itemquantity
+
+    return render_template('items/add_or_update.html', form=form)
--- app/mappers/item_mapper.py
+++ app/mappers/item_mapper.py
@@ -1,4 +1,5 @@
 from app.dtos.item_dto import ItemDTO
+from app.forms.item.item_form import ItemForm
 from app.mappers.abstract_mapper import AbstractMapper
 from app.models.item import Item
 
@@ -6,8 +7,13 @@ from app.models.item import Item
 class ItemMapper(AbstractMapper):
     @staticmethod
     def entity_to_dto(item: Item):
-        ItemDTO.build_from_entity(item)
+        return ItemDTO.build_from_entity(item)
 
     @staticmethod
     def form_to_entity(form, item: Item):
-        pass
+        if isinstance(form, ItemForm):
+            item.itemname = form.itemname.data
+            item.itemdescription = form.itemdescription.data
+            item.itemstock = form.itemstock.data
+
+        return item
