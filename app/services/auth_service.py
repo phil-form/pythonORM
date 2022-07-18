dc72f9a002142cc72f438557abdf8996da06051b
philippe.stons <philippe.stons@bstorm.be>
2022-07-18 14:54:00 +0200

    [DEPENDENCY INJECTION]

---
+++ app/services/auth_service.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+from app.dtos.user_dto import UserDTO
+
+
+class AuthService(ABC):
+    def __init__(self):
+        print("INIT AUTH SERVICE")
+
+    def __del__(self):
+        print("DELETE AUTH SERVICE")
+
+    @abstractmethod
+    def get_current_user(self) -> UserDTO:
+        pass
