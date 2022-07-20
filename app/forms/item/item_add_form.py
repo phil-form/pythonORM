fde16350823471ea8a03f6d46542a3e56871b25e
clbr-odoo <clbr@odoo.com>
2022-07-20 11:44:43 +0200

    [ADD] JS table for items and injectors for services in controllers

--- app/forms/item/item_add_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
-
-
-class ItemAddForm(FlaskForm):
-    itemname = StringField('username', validators=[DataRequired()])
-    itemdescription = StringField('username', validators=[DataRequired()])
-    itemstock = IntegerField('itemstock', validators=[DataRequired()])
