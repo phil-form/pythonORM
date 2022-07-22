b9dcd143d2826508975a1d37543c7db18cde3678
dszklarz <david.szklarzewski@gmail.com>
2022-07-22 15:56:22 +0200

    b4 merge

--- app/controllers/basket_controller.py
+++ app/controllers/basket_controller.py
@@ -1,20 +1,16 @@
-from flask import render_template, session, request, redirect, url_for
+from flask import jsonify, render_template, session, request, redirect, url_for
 
 from app import app
 from app.framework.decorators.auth_required import auth_required
-from app.forms.basket.basket_add_item_form import BasketAddItemForm
 from app.framework.decorators.inject import inject
-from app.services.basket_service import BasketService
-from app.services.item_service import ItemService
-from app.services.user_service import UserService
 
-basketService = BasketService()
-itemService = ItemService()
-userService = UserService()
+from app.forms.basket.basket_add_item_form import BasketAddItemForm
+from app.services.basket_service import BasketService
 
 @app.route('/basket/all')
 @auth_required(level="ADMIN")
-def getAllBaskets():
+@inject
+def getAllBaskets(basketService: BasketService):
     return render_template('baskets/list.html', baskets=basketService.find_all())
 
 @app.route('/basket')
@@ -27,7 +23,8 @@ def getBasketDetail(basketService: BasketService):
 
 @app.route('/basket/add', methods=['POST'])
 @auth_required()
-def add_item_to_basket():
+@inject
+def add_item_to_basket(basketService: BasketService):
     item_to_add = BasketAddItemForm(request.form)
 
     basketService.add_item(item_to_add)
@@ -36,14 +33,22 @@ def add_item_to_basket():
 
 @app.route('/basket/remove/<int:itemid>', methods=['POST'])
 @auth_required()
-def remove_item_to_basket(itemid: int):
+@inject
+def remove_item_to_basket(basketService: BasketService, itemid: int):
     basketService.remove_item(itemid)
 
     return redirect(url_for('getBasketDetail'))
 
 @app.route('/basket/checkout', methods=['POST'])
 @auth_required()
-def checkout_basket():
+@inject
+def checkout_basket(basketService: BasketService):
     basketService.checkout_basket()
 
     return redirect(url_for('getBasketDetail'))
+
+@app.route('/api/basket/<int:basketid>', methods=['GET'])
+@auth_required()
+@inject
+def getBasketFromApi(basketservice: BasketService, basketid: int):
+    return jsonify([item.get_json_parsable() for item in basketservice.find_one(basketid).items])
